f3634b550305b7c229ffff7b637579a3c2a39e7b
Khaled <khaled.romeh@archerirm.com>
2025-02-01 01:26:59 +0200

    Updated ui url in cors

--- src/main/java/com/spotify/rewrapped/configs/CorsConfig.java
+++ src/main/java/com/spotify/rewrapped/configs/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.spotify.rewrapped.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    @Value("${ui.url}")
+    private String UI_URL;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React dev server
+                        .allowedOrigins(UI_URL)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // For cookies/auth headers
